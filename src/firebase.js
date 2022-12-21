dc98f2879c137ee857f8c69003e829bbb7329471
rolanntig <106159101+rolanntig@users.noreply.github.com>
2022-12-21 10:37:25 +0100

    df

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCv2ku9Uq-psTbBJ0xtXQ6VG0wwa3tdkd4",
+  authDomain: "weatherapp-2de2b.firebaseapp.com",
+  projectId: "weatherapp-2de2b",
+  storageBucket: "weatherapp-2de2b.appspot.com",
+  messagingSenderId: "912276131401",
+  appId: "1:912276131401:web:f050524e91220ad4d97998"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
