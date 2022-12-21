19ebfa210a8ffef1c922dfc3a616d72154057843
rolanntig <106159101+rolanntig@users.noreply.github.com>
2022-12-21 09:40:15 +0100

    jnasdn

--- WeatherApp/src/App.jsx
+++ WeatherApp/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { city } from './countries'
-import {Autocomplete, TextField, Card, CardContent, CardActions, Typography} from '@mui/material'
+import {Autocomplete, TextField, Card, CardContent, CardActions, Typography, Button,Box, createTheme} from '@mui/material'
 import { useState } from 'react'
 import axios from 'axios'
 
@@ -10,58 +10,48 @@ function App() {
   
   const [value, setValue] = useState(city[0]);
   const [inputValue, setInputValue] = useState('');
-  const [weathers,setWeathers] = useState('');
   const [cards,setCards] = useState([]);
+  const [expandedState, setExpandedState] = useState({});
+  
 
+  const CardGen = (id) => {
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${value.label}&appid=05abf85529bfdb321b2ca9f97f52d0b8`)
+    .then(res =>{
+        let temp = Math.ceil(res.data.main.temp - 272.15);
+        let icon = `http://openweathermap.org/img/wn/${res.data.weather[0].icon}@2x.png`
+        console.log(res);
+        setCards([
+          ...cards,
+          <Card key={value.label} sx={{ width: 250, ml:10 }} className="card">
+            <CardContent>
+                <Box>
+                  <Box className='icons'>
+                  <Typography variant='h4'>{value.label}</Typography>
+                  <img src={icon} alt="icon" />
+                  </Box>
+                  <Typography variant='h5'>Temp: {temp}C°</Typography>
+                  <Typography variant='h6'>Weather: {res.data.weather[0].main}</Typography>
+                  <Typography variant='h6'>Feels Like: {res.data.main.feels_like}</Typography>
+                  <Typography variant='h6'>Max Temp: {res.data.main.temp_max}</Typography>
+                  <Typography variant='h6'>Min Temp: {res.data.main.temp_min}</Typography>
+                  <Typography variant='h6'>Humidity: {res.data.main.humidity}%</Typography>
+                  <Typography variant='h6'>Pressure: {res.data.main.pressure} hPa</Typography>
+                  <Typography variant='h6'>Wind Speed: {res.data.wind.speed} m/s</Typography>
+                </Box>
+            </CardContent>
+            </Card>
+        ])
+      }
+    )}
 
-
-
-    const CardGen = () =>{
-      getWeather();
-      setCards([
-        ...cards,
-      
-        <Card sx={{ maxWidth: 200, ml: 20 }}>
-        <CardContent>
-          <Typography variant='h4'>{value.label}</Typography>
-          <Typography variant='h5'>{weathers.main}</Typography>
-          <Typography varient='h5'>{weathers.weather.icon}</Typography>
-          <Typography variant='h6'>{weathers.weather.main}</Typography>
-        </CardContent>
-        <CardActions>
-          <button>Description</button>
-        </CardActions>
-        </Card>,
-      ])
-
-  }
-
- 
-
-
-
-
-
-console.log(weathers);
-
-  const getWeather = () => {
-    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${value.label}&appid=05abf85529bfdb321b2ca9f97f52d0b8
-    `)
-      .then(res =>{
-      setWeathers(res.data);
-      console.log(res.data);
-    })
-    .catch(error => {
-      // handle error
-      console.log(error);
-    })
-  }
+  
 
 
   return (
+    <>
     <div>
     
-      <h1>Weder epp</h1>
+      <h1>Väder App</h1>
       <div className='Searchbar'>
       <Autocomplete
         value={value}
@@ -71,14 +61,23 @@ console.log(weathers);
         id="city"
         options={city}
         sx={{ width: 300 }}
-        renderInput={(params) => <TextField {...params} label="city" />}
+        renderInput={(params) => <TextField {...params} label="city"/>}
       />
       </div>
-        <button onClick={CardGen}>gdsjkf</button>
-        {cards}
+      <Box textAlign="center">
+        <Button onClick={CardGen} variant="contained" color='warning' sx={{width:300, mt:2,}}>Generate</Button>
+      </Box>
     </div>
+
+    <div>
+      <Box sx={{mt:5}}>
+        {cards}
+      </Box>
+    </div>
+    </>
   )
 }
+  
 
   
 
